a6d1c640acfe637ac25b298f21667cfc551a508e
anishnairva <anishnairva@gmail.com>
2023-11-21 23:20:03 +0530

    added new 3 ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("code for ml models")
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model2")
+
+def model3():
+    print("adding code for model3")
